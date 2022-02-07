891cde7640f7919b7dfc16c152a5c36155f48ca0
frankljin <52306663+frankljin@users.noreply.github.com>
2022-02-06 19:43:19 -0500

    Update 1828.cpp

--- Leetcode Problems/1828.cpp
+++ Leetcode Problems/1828.cpp
@@ -5,10 +5,9 @@ public:
         for (auto q : queries) {
             int count = 0;
             for (auto p : points) {
-                int x = (p[0] - q[0])*(p[0] - q[0]);
-                int y = (p[1] - q[1])*(p[1] - q[1]);
-                int r = q[2]*q[2];
-                
+                int x = (p[0] - q[0]) * (p[0] - q[0]);
+                int y = (p[1] - q[1]) * (p[1] - q[1]);
+                int r = q[2] * q[2];  
                 if (x + y <= r) {
                     count += 1;
                 }
